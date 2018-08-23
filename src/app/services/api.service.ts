ec1cb4290e9268762d8287d27b2d2a4a437e5350
Maikel Arcia Carrazana <macarci@gmail.com>
2018-08-23 12:38:01 -0300

    Improving API request with options

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -4,6 +4,12 @@ import {Observable, Observer} from 'rxjs';
 import {environment} from '../../environments/environment';
 import {AuthService} from './auth.service';
 
+export interface RequestOptions {
+  headers?: Object;
+  query?: Object;
+  template?: Object;
+}
+
 @Injectable()
 export class ApiService {
 
@@ -12,64 +18,74 @@ export class ApiService {
     readonly authService: AuthService) {
   }
 
-  get<T>(params: string[], options?: { headers?: Object; query?: Object; template?: Object }): Observable<T> {
+  get<T>(params: string[], options?: RequestOptions): Observable<T> {
     options = options || {};
     return new Observable<T>((subscriber: Observer<T>) => {
-      this.authService.getAccessToken().then(
-        access_token => {
-          const reqOptions = {
-            headers: {
-              Authorization: 'Bearer ' + access_token
-            }
-          };
-          if (options.query) {
-            reqOptions['params'] = options.query;
-          }
-          const templateOptions = options.template || {};
-          if (!templateOptions.hasOwnProperty('raw_properties')) {
-            templateOptions['raw_properties'] = true;
-          }
-          reqOptions.headers['X-Template-Options'] = JSON.stringify(templateOptions);
-          this.httpClient.get(this.apiURL(params), reqOptions).subscribe(
-            (response: T) => {
-              subscriber.next(response);
-            },
-            error => {
-              subscriber.error(error);
-            },
-            () => subscriber.complete()
-          );
-        }).catch(error => subscriber.error(error));
+      this.httpRequestOptionsFrom(options)
+        .then(
+          reqOptions => {
+            this.httpClient.get(this.apiURL(params), reqOptions)
+              .subscribe(
+                (response: T) => {
+                  subscriber.next(response);
+                },
+                error => {
+                  subscriber.error(error);
+                },
+                () => subscriber.complete()
+              );
+          })
+        .catch(error => subscriber.error(error));
     });
   }
 
-  post(params: string[], body: any, query?: { [param: string]: string | string[] }): Observable<Object> {
+  post(params: string[], body: any, options?: RequestOptions): Observable<Object> {
     return new Observable<Object>(subscriber => {
-      this.authService.getAccessToken().then(
-        access_token => {
-          this.httpClient.post(this.apiURL(params), body, {
-            headers: {
-              Authorization: 'Bearer ' + access_token
-            },
-            params: query
-          }).subscribe(
-            response => {
-              subscriber.next(response);
-            },
-            error => {
-              subscriber.error(error);
-            },
-            () => subscriber.complete()
-          );
-        }).catch(error => subscriber.error(error));
+      this.httpRequestOptionsFrom(options)
+        .then(
+          reqOptions => {
+            this.httpClient.post(this.apiURL(params), body, reqOptions)
+              .subscribe(
+                response => {
+                  subscriber.next(response);
+                },
+                error => {
+                  subscriber.error(error);
+                },
+                () => subscriber.complete()
+              );
+          })
+        .catch(error => subscriber.error(error));
     });
-  }
-
-  digest(params: string[], body: any = '', query?: { [param: string]: string | string[] }): Observable<Object> {
-    return this.post([...params, 'digest'], body, query);
   }
 
   apiURL(params: string[]) {
     return environment.cenitHost + '/api/v3/' + params.join('/');
+  }
+
+  httpRequestOptionsFrom(options: RequestOptions): Promise<Object> {
+    return new Promise<Object>(
+      (resolve, reject) => {
+        options = options || {};
+        this.authService.getAccessToken().then(
+          access_token => {
+            const reqOptions = {
+              headers: {
+                Authorization: 'Bearer ' + access_token
+              }
+            };
+            if (options.query) {
+              reqOptions['params'] = options.query;
+            }
+            const templateOptions = options.template || {};
+            if (!templateOptions.hasOwnProperty('raw_properties')) {
+              templateOptions['raw_properties'] = true;
+            }
+            reqOptions.headers['X-Template-Options'] = JSON.stringify(templateOptions);
+            resolve(reqOptions);
+          }
+        ).catch(error => reject(error));
+      }
+    );
   }
 }
