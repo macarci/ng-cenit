9a640bd89ee672cd37885bd66c7464557b2f2347
Maikel Arcia Carrazana <macarci@gmail.com>
2018-08-24 12:38:59 -0300

    Dynamic main navigation (pilot)

--- src/app/navigation/main-navigation/main-navigation.component.ts
+++ src/app/navigation/main-navigation/main-navigation.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {ApiService} from '../../services/api.service';
 
 @Component({
   selector: 'cenit-navigation-main',
@@ -10,46 +11,36 @@ export class MainNavigationComponent implements OnInit {
   @Input() showLabels = false;
   @Input() switchLabelsEnabled = true;
 
+  defaultTypes = [];
+  types = this.defaultTypes;
   items = [
     {
       title: 'Types',
       icon: 'widgets',
-      items: [{
-        title: 'Object Types',
-        icon: 'class',
-        path: '/json_data_type'
-      }, {
-        title: 'File Types',
-        icon: 'folder',
-        path: '/file_data_type'
-      }]
+      items: this.types
     },
     {
-      title: 'Connectors',
-      icon: 'category',
+      title: 'Config',
+      icon: 'build',
       items: [{
-        title: 'Connections',
-        icon: 'settings_input_component',
-        path: '/connection'
+        title: 'Namespaces',
+        icon: 'waves',
+        path: '/namespace'
       }, {
-        title: 'Channels',
-        icon: 'import_export',
-        path: '/cenit_ui~refresh_token'
-      },{
-        title: 'Channel Item',
-        icon: 'import_export',
-        path: '/cenit_ui~refresh_token/1'
-      },{
-        title: 'Dashboard',
-        icon: 'dashboard',
-        path: '/dashboard'
+        title: 'Data Types',
+        icon: 'class',
+        path: '/data_type_config'
       }]
     }];
+
+  constructor(private apiService: ApiService) {
+  }
 
   ngOnInit() {
     if (!this.switchLabelsEnabled) {
       this.showLabels = true;
     }
+    this.loadTypes();
   }
 
   setSwitchingEnable(enable) {
@@ -64,4 +55,41 @@ export class MainNavigationComponent implements OnInit {
     }
   }
 
+  loadTypes() {
+    const handleError = (error) => {
+      this.items[0]['items'] = this.types = this.defaultTypes;
+    };
+    this.apiService.get(['setup', 'data_type_config'], {
+      query: {navigation_link: true},
+      template: {viewport: '{slug data_type{namespace title}}'}
+    }).subscribe(
+      configs => {
+        this.apiService.get(['setup', 'namespace'])
+          .subscribe(
+            namespaces => {
+              this.items[0]['items'] = this.types = this.defaultTypes.concat(
+                configs['items'].map(
+                  config => {
+                    const namespace = namespaces['items'].find(
+                      ns => ns['name'] === config['data_type']['namespace']
+                    );
+                    if (namespace) {
+                      return {
+                        title: namespace['name'] + ' | ' + config['data_type']['title'],
+                        icon: 'category',
+                        path: '/' + namespace['slug'] + '~' +
+                        '' + config['slug']
+                      };
+                    }
+                    return null;
+                  }
+                ).filter(item => item)
+              );
+            },
+            handleError
+          );
+      },
+      handleError
+    );
+  }
 }
